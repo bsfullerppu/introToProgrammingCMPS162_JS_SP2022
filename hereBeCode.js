a7c6c2ea8e51515a430d323f107b6791a705ce48
Benjamin Fuller <72768917+bsfullerppu@users.noreply.github.com>
2022-03-07 11:17:49 -0500

    Add files via upload

---
+++ hereBeCode.js
@@ -0,0 +1,3 @@
+function hideImage() {
+  document.getElementById("roma").className = "hidden";
+}
---
+++ textAlterationFunction.js
@@ -0,0 +1,12 @@
+function enlargeText() {
+  document.getElementById("lotrParagraph").style.fontSize = "50px";
+}
+function decreaseText() {
+  document.getElementById("lotrParagraph").style.fontSize = "10px";
+}
+function enlargeTextHeadingOne() {
+  document.getElementById("openingHeading").style.fontSize = "50px";
+}
+function decreaseTextHeadingOne() {
+  document.getElementById("openingHeading").style.fontSize = "10px";
+}
